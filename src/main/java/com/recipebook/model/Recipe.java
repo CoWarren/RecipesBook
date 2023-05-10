2caf50c6fa8c8ac599fa00fa8dc0fde7c19047be
Chris-warren <chris-warrentodd@hollandandbarrett.com>
2023-05-10 16:11:16 +0100

    created recipe model using lombok

--- src/main/java/com/recipebook/model/Recipe.java
+++ src/main/java/com/recipebook/model/Recipe.java
@@ -1,13 +1,33 @@
 package com.recipebook.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+// Lombok is a framework that tells java to create getter, setters and no args constructor
 import java.util.Date;
-
-
+@Getter
+@Setter
+@NoArgsConstructor
 public class Recipe {
-    //TODO create
     private String title;
+
     private String description;
+
     private Date dateAdded;
 
+    public Recipe(String title, String description, Date dateAdded){
+        this.title = title;
+        this.description = description;
+        this.dateAdded = dateAdded;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", dateAdded=" + dateAdded +
+                '}';
+    }
 
 }
