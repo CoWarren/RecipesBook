75953f57f56d36ac43a8df0c96a64f4e7e630499
Obin Gold <individualog@gmail.com>
2023-05-10 17:59:45 +0100

    Trying to fix Controller (#14)
    * Trying to fix Controller
    
    * fixed file structure and added routes
    
    ---------
    
    Co-authored-by: Obin Gurung <obingurung@hollandandbarrett.com>
    Co-authored-by: Chris-warren <chris-warrentodd@hollandandbarrett.com>

---
+++ src/main/java/com/recipebook/recipebook/controller/RecipeController.java
@@ -0,0 +1,29 @@
+package com.recipebook.recipebook.controller;
+
+import com.recipebook.recipebook.model.Recipe;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RequestMapping("/api/recipes")
+@RestController
+public class RecipeController {
+
+    private final List<Recipe> recipes; // List to store recipes (in-memory representation)
+
+    public RecipeController() {
+        recipes = new ArrayList<>();
+        // Add some sample recipes for testing
+        recipes.add(new Recipe("Pancakes", "Flour, milk, eggs", LocalDate.of(1999,2, 1)));
+    }
+
+    @GetMapping
+    public @ResponseBody List<Recipe> getAllRecipes() {
+        return recipes;
+    }
+}
+
