bf0800418037454da8c007ebb863a09e245b8b0e
Obin Gurung <obingurung@hollandandbarrett.com>
2023-05-10 16:11:38 +0100

    Created RecipeController GET method with generic newArrayList of recipes

--- src/main/java/com/recipebook/controller/RecipeController.java
+++ src/main/java/com/recipebook/controller/RecipeController.java
@@ -1,7 +1,29 @@
 package com.recipebook.controller;
 
 import com.recipebook.model.Recipe;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("")
 public class RecipeController {
-    //TODO
-}
+
+        private List<Recipe> recipes; // List to store recipes (in-memory representation)
+
+        public RecipeController() {
+            recipes = new ArrayList<>();
+            // Add some sample recipes for testing
+            recipes.add(new Recipe("Pancakes", "Flour, milk, eggs", "Mix ingredients and cook on a pan"));
+            recipes.add(new Recipe("Spaghetti Bolognese", "Ground beef, tomatoes, pasta", "Cook beef, add tomatoes, serve with pasta"));
+        }
+
+        @GetMapping
+        public List<Recipe> getAllRecipes() {
+            return recipes;
+        }
+
+    }
