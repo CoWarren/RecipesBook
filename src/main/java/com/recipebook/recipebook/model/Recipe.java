61ba87f2666797759f85121fe8583dd10194acf4
obingurung <obingurung@hollandandbarrett.com>
2023-05-17 16:13:29 +0100

    Refactored Recipe model with Project Lombok annotations

--- src/main/java/com/recipebook/recipebook/model/Recipe.java
+++ src/main/java/com/recipebook/recipebook/model/Recipe.java
@@ -1,34 +1,21 @@
 package com.recipebook.recipebook.model;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 // Lombok is a framework that tells java to create getter, setters and no args constructor
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
-@Getter
-@Setter
+@Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Recipe {
 
     private int recipeId;
     private String title;
     private String description;
     private LocalDate dateAdded;
-
-    public Recipe(int recipeId, String title, String description, LocalDate dateAdded){
-        this.recipeId = recipeId;
-        this.title = title;
-        this.description = description;
-        this.dateAdded = dateAdded;
-    }
-
-    @Override
-    public String toString() {
-        return "Recipe{" +
-                "recipeId=" + recipeId +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", dateAdded=" + dateAdded +
-                '}';
-    }
 }
