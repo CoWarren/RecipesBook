365281204d1b50b440bcfc73e05fd3d2eb2b82d4
Obin Gold <individualog@gmail.com>
2023-05-17 18:02:28 +0100

    Built POST method for Recipes (#47)
    Co-authored-by: obingurung <obingurung@hollandandbarrett.com>

--- src/main/java/com/recipebook/recipebook/service/RecipeService.java
+++ src/main/java/com/recipebook/recipebook/service/RecipeService.java
@@ -4,7 +4,6 @@ import com.recipebook.recipebook.model.Recipe;
 import com.recipebook.recipebook.repository.InMemoryRecipeRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Map;
 
 @Service // Annotation must be implemented for Spring Boot to autowire inMemoryRepository
@@ -16,15 +15,21 @@ public class RecipeService {
         this.inMemoryRecipeRepository = inMemoryRecipeRepository;
     }
 
-    public Map<Integer,Recipe> getAllRecipes() {
+    public Map<String,Recipe> getAllRecipes() {
         return inMemoryRecipeRepository.getAllRecipes();
     }
 
-    public Recipe getRecipeById(int recipeId) {
+    public Recipe getRecipeById(String recipeId) {
         return inMemoryRecipeRepository.getRecipeById(recipeId);
     }
 
-    public boolean deleteRecipeById(int recipeId) {
+    public Recipe createRecipe(Recipe recipe) {
+        return inMemoryRecipeRepository.createRecipe(recipe);
+    }
+
+    public boolean deleteRecipeById(String recipeId) {
         return inMemoryRecipeRepository.deleteRecipeById(recipeId);
     }
+
+
 }
