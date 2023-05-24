0f1334698976148c200c6684a078d3595858deef
Chris-warren <chris-warrentodd@hollandandbarrett.com>
2023-05-24 16:33:13 +0100

    created get mappings and added service annotation

--- src/main/java/com/recipebook/recipebook/controller/UserController.java
+++ src/main/java/com/recipebook/recipebook/controller/UserController.java
@@ -1,5 +1,30 @@
 package com.recipebook.recipebook.controller;
 
+import com.recipebook.recipebook.model.User;
+import com.recipebook.recipebook.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RequestMapping("/users")
+@RestController
 public class UserController {
-    //TODO
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public @ResponseBody Map<Integer, User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("{userId}")
+    public @ResponseBody User getUserById(@PathVariable("userId") int userId) {
+        return userService.getUserById(userId);
+    }
+//
+//    @GetMapping
+//    public @ResponseBody Map<i>
 }
